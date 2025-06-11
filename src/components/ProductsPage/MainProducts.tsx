157b7b482362c0a4decc2d44c01e2a9f00d09e0b
ahmed <ahmedabozaid.dev@gmail.com>
2025-06-11 16:34:19 +0300

    adds products filter

--- src/components/ProductsPage/MainProducts.tsx
+++ src/components/ProductsPage/MainProducts.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ProductCard from "./ProductCard";
 import ProductsFilter from "./ProductsFilter";
 import ProductsSearchbar from "./ProductsSearchbar";
 
 const MainProducts = () => {
+  const [selectedFilter, setSelectedFilter] = useState<string>("");
   return (
     <main>
       <h1 className="font-bold text-2xl text-center mt-8 sm:mt-16">المنتجات</h1>
       <ProductsSearchbar />
-      <ProductsFilter />
+      <ProductsFilter
+        selectedFilter={selectedFilter}
+        setSelectedFilter={setSelectedFilter}
+      />
       <div>
         <ProductCard />
         <ProductCard />
--- src/components/ProductsPage/ProductsFilter.tsx
+++ src/components/ProductsPage/ProductsFilter.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
 
-const ProductsFilter = () => {
-  return (
-    <div>ProductsFilter</div>
-  )
+const filters = [
+  {
+    key: "",
+    titleEn: "All",
+    titleAr: "الكل",
+  },
+  {
+    key: "digital_work_card",
+    titleEn: "Digital Work Card",
+    titleAr: "بطاقة العمل الرقمية",
+  },
+  {
+    key: "digital_key_series",
+    titleEn: "Digital Key Series",
+    titleAr: "سلسلة المفاتيح الرقمية",
+  },
+  {
+    key: "advanced_wallet_booklet",
+    titleEn: "Advanced Wallet Booklet",
+    titleAr: "بوكت المحفظة المتطورة",
+  },
+  {
+    key: "digital_sticker",
+    titleEn: "Digital Sticker",
+    titleAr: "الملصق الرقمي",
+  },
+  {
+    key: "custom_work_card",
+    titleEn: "Custom Work Card",
+    titleAr: "بطاقة العمل المخصصة",
+  },
+];
+
+interface ProductsFilterProps {
+  selectedFilter: string;
+  setSelectedFilter: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default ProductsFilter
+const ProductsFilter = ({
+  selectedFilter,
+  setSelectedFilter,
+}: ProductsFilterProps) => {
+  return (
+    <div className="container flex items-center justify-between flex-wrap gap-5">
+      {filters.map((filter) => (
+        <button
+          key={filter.titleEn}
+          onClick={() => setSelectedFilter(filter.key)}
+          className={`py-2 px-4 cursor-pointer ${
+            selectedFilter === filter.key
+              ? "bg-main-blue text-white rounded-full"
+              : ""
+          }`}
+        >
+          {filter.titleAr}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default ProductsFilter;
