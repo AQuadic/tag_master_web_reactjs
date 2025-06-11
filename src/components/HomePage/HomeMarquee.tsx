e20568651122ee781c310f503c9ef43f5e420ca2
ahmed <ahmedabozaid.dev@gmail.com>
2025-06-11 10:22:11 +0300

    fixes home marquee

--- src/components/HomePage/HomeMarquee.tsx
+++ src/components/HomePage/HomeMarquee.tsx
@@ -2,20 +2,22 @@ import { homeMarqueeImages } from "@/constants/home/homeMarqueeImages";
 import Image from "next/image";
 import React from "react";
 import Marquee from "react-fast-marquee";
+import StarIcon from "../icons/home/StarIcon";
 
 const HomeMarquee = () => {
   return (
-    <div className="w-full overflow-hidden">
-      <Marquee
-        speed={40}
-        gradient={false}
-        play={true}
-        direction="left"
-        autoFill
-      >
+    <div className="w-full overflow-hidden my-10 sm:my-20" dir="ltr">
+      <Marquee speed={40} gradient={false} play={true} autoFill>
         {homeMarqueeImages.map((image, idx) => (
-          <div key={idx} className="mx-8">
-            <Image src={image} width={44} height={44} alt="marquee" />
+          <div key={idx} className="flex items-center">
+            <Image
+              src={image}
+              width={44}
+              height={44}
+              alt="marquee"
+              className="mx-20"
+            />
+            <StarIcon />
           </div>
         ))}
       </Marquee>
