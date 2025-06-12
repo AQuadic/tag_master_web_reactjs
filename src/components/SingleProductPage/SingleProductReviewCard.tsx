d0e943c5fd4225a4a54d7f4c2375607e7381f01b
ahmed <ahmedabozaid.dev@gmail.com>
2025-06-12 16:58:15 +0300

    adds review structure

---
+++ src/components/SingleProductPage/SingleProductReviewCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleProductReviewCard = () => {
+  return (
+    <div>SingleProductReviewCard</div>
+  )
+}
+
+export default SingleProductReviewCard
---
+++ src/components/SingleProductPage/SingleProductReviews.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleProductReviews = () => {
+  return <div>SingleProductReviews</div>;
+};
+
+export default SingleProductReviews;
