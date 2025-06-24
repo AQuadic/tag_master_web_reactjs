4f053fe5222b17a46bd37c376abf2e9849091a7a
ahmed <ahmedabozaid.dev@gmail.com>
2025-06-24 16:02:48 +0300

    designs login form

--- src/components/auth/login/MainLogin.tsx
+++ src/components/auth/login/MainLogin.tsx
@@ -9,17 +9,15 @@ const LoginStep1 = () => {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   return (
-    <section className="flex flex-col items-center justify-center  container !max-w-[400px]">
+    <section className="flex flex-col items-center justify-center  container !max-w-[400px]  gap-5 sm:gap-8 my-10 sm:my-20">
       <Image src="/images/logo.png" alt="Logo" width={155} height={54} />
       <div className="flex flex-col gap-3">
-        <h1 className="font-bold text-2xl text-center my-5 sm:my-8">
-          تسجيل الدخول
-        </h1>
+        <h1 className="font-bold text-2xl text-center">تسجيل الدخول</h1>
         <p className="text-lg text-secondary-text text-center">
           أدخل البريد الاكتروني و كلمة المرور الخاصة بك لتسجيل الدخول إلى حسابك.
         </p>
       </div>
-      <form className=" w-full">
+      <form className=" w-full flex flex-col gap-5 sm:gap-8">
         <Input
           className="border-neutral-700 w-full   "
           value={email}
@@ -33,9 +31,9 @@ const LoginStep1 = () => {
           onChange={(e) => setPassword(e.target.value)}
           placeholder="كلمة المرور"
         />
-        <Button></Button>
+        <Button>تسجيل الدخول</Button>
       </form>
-      <Link className="text-primary text-lg" href="/auth/signup">
+      <Link className="text-primary text-lg self-start" href="/auth/signup">
         ليس لديك حساب؟
       </Link>
     </section>
