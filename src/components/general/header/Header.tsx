3c4c86654fdc2c174b64d2b36a5e4fed8c67078d
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-07-02 15:36:37 +0300

    add cart counter

--- src/components/general/header/Header.tsx
+++ src/components/general/header/Header.tsx
@@ -12,11 +12,16 @@ import Searchbar from "./Searchbar";
 import SignInButton from "./SignInButton";
 import { useAuthStore } from "@/components/stores/userStore";
 import Profile from "@/components/icons/general/Profile";
+import { useCartStore } from "@/components/stores/cartStore";
 
 const Header = () => {
   const pathname = usePathname();
   const user = useAuthStore((state) => state.user);
   const isSignedIn = !!user;
+const cart = useCartStore((state) => state.cart);
+
+const itemsCount =
+  cart?.cart_items?.reduce((sum, item) => sum + item.quantity, 0) || 0;
 
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
@@ -144,9 +149,13 @@ const Header = () => {
                   className=" rounded-xl hover:bg-gray-100 transition-all duration-200 relative group"
                 >
                   <CartIcon />
-                  <div className="absolute w-5 h-5 rounded-full bg-[#FF3B30] text-[#FFFFFF] flex items-center justify-center -top-2 -left-1">
-                    <p>2</p>
-                  </div>
+          
+                {itemsCount > 0 && (
+                    <div className="absolute w-5 h-5 rounded-full bg-[#FF3B30] text-[#FFFFFF] flex items-center justify-center -top-2 -left-1 text-xs">
+                      <p>{itemsCount > 9 ? "9+" : itemsCount}</p>
+                    </div>
+                  )}
+
                 </Link>
               </motion.div>
             )}
--- src/components/stores/cartStore.ts
+++ src/components/stores/cartStore.ts
@@ -1,17 +1,61 @@
 import { create } from "zustand";
-import { addToCart } from "@/api/cart/addToCart"; 
+import { addToCart } from "@/api/cart/addToCart";
+import { getCart } from "@/api/cart/getCart"; 
+import { toast } from "sonner";
 
-interface CartState {
-  addToCart: (itemId: number) => Promise<void>;
+interface CartItem {
+  id: number;
+  itemable_id: number;
+  price: number;
+  quantity: number;
 }
 
-export const useCartStore = create<CartState>(() => ({
+interface Cart {
+  cart_items: CartItem[];
+  total_price?: number;
+  final_total?: number;
+  discount_amount?: number;
+  tax_value?: number;
+}
+
+interface CartState {
+  cart: Cart | null;
+  coupon: string;
+  setCoupon: (coupon: string) => void;
+  getCart: () => Promise<void>;
+  addToCart: (itemId: number) => Promise<void>;
+  getItemsCount: () => number;
+}
+
+export const useCartStore = create<CartState>((set, get) => ({
+  cart: null,
+  coupon: "",
+
+  setCoupon: (coupon) => set({ coupon }),
+
+  getCart: async () => {
+    try {
+      const coupon = get().coupon;
+      const cartData = await getCart(coupon);
+      set({ cart: cartData });
+    } catch (error) {
+      console.error("Failed to fetch cart", error);
+    }
+  },
+
   addToCart: async (itemId: number) => {
     try {
       const response = await addToCart(itemId);
       console.log("Added to cart:", response);
+      await get().getCart()
     } catch (error) {
       console.error("Failed to add product to cart", error);
+      toast.error("فشل في إضافة المنتج للسلة");
     }
   },
+
+    getItemsCount: () => {
+        const { cart } = get();
+        return cart?.cart_items?.length || 0;
+      },
 }));
