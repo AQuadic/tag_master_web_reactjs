fde93eddc3aace6b1e7954c8bc67a028d9354c79
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-07-08 12:56:48 +0300

    add and remove from bookmarks

--- src/components/blog/Blogs.tsx
+++ src/components/blog/Blogs.tsx
@@ -1,19 +1,15 @@
+"use client";
+
+import React from 'react'
 import Image from 'next/image'
-import React, { useState } from 'react'
-import Bookmark from '../icons/blogs/Bookmark'
-import { getPosts } from '@/api/blogs/getBlogs'
-import { PostType } from '@/types/blogs'
-import { useQuery } from '@tanstack/react-query'
 import Spinner from '../icons/general/Spinner'
-import Link from 'next/link'
-import { removeFromFavorite } from '@/api/favorite/removFromFav'
-import { toast } from 'sonner'
-import { addToFavorite } from '@/api/favorite/addToFav'
-import FilledBookmarkIcon from '../icons/blogs/FilledBookmarkIcon'
+import { getPosts } from '@/api/blogs/getBlogs'
+import { useQuery } from '@tanstack/react-query'
+import { PostType } from '@/types/blogs'
+import BlogCard from './BlogCard'
 import { useTranslations } from 'next-intl'
 
-const Blogs = ({ post }: { post: PostType })  => {
-// const [isFavorite, setIsFavorite] = useState(post.is_favorite);
+const Blogs = () => {
   const t = useTranslations("homeblogs");
 
   const {
@@ -24,93 +20,33 @@ const Blogs = ({ post }: { post: PostType })  => {
     queryFn: getPosts,
   });
 
-  if (isLoading) return <div className='flex justify-center my-10'>
-    <Spinner />
-  </div>
+  if (isLoading) return <div className='flex justify-center my-10'><Spinner /></div>
 
+  return (
+    <section className='mt-6'>
+      <div className='relative'>
+        <Image 
+          src='/images/blogs/blogImage.png' 
+          alt='Blog image' 
+          width={1200} 
+          height={453} 
+          className='object-cover w-full'
+        />
+        <div className='absolute bottom-4 right-8 md:p-6 text-white'>
+          <h2 className='md:text-5xl text-xl font-bold'>{t('smartTools')}</h2>
+          <p className='md:w-[570px] md:text-2xl text-base font-normal mt-4'>
+            {t('description')}
+          </p>
+        </div>
+      </div>
 
-// const handleToggleFavorite = async (e: React.MouseEvent<HTMLButtonElement>) => {
-//   e.preventDefault();
-//   try {
-//     if (isFavorite) {
-//       await removeFromFavorite({
-//         favorable_id: post.id,
-//         favorable_type: "blog",
-//       });
-//       setIsFavorite(false);
-//       toast.success("تمت إزالة المقال من المحفوظات");
-//     } else {
-//       await addToFavorite({
-//         favorable_id: post.id,
-//         favorable_type: "blog",
-//       });
-//       setIsFavorite(true);
-//       toast.success("تمت إضافة المقال إلى المحفوظات");
-//     }
-//   } catch (error: any) {
-//     toast.error("حدث خطأ أثناء تحديث المحفوظات");
-//     console.error("Toggle favorite error:", error);
-//   }
-// };
-
-
-    return (
-        <section className='mt-6'>
-            <div className='relative'>
-                <Image 
-                src='/images/blogs/blogImage.png' 
-                alt='Blog image' 
-                width={1200} 
-                height={453} 
-                className='object-cover w-full'
-            />
-                <div className='absolute bottom-4 right-8 md:p-6 text-white'>
-                    <h2 className='md:text-5xl text-xl font-bold'>{t('smartTools')}</h2>
-                    <p className='md:w-[570px] md:text-2xl text-base font-normal mt-4'>
-                        {t('description')}
-                    </p>
-                </div>
-            </div>
-
-            <div className='mt-12 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 items-center justify-center gap-[21px]'>
-                {posts?.map((item,index) => (
-                    <Link href={`/blog/${item.id}`} key={item.id}>
-                    <div
-                        key={index}
-                        className='relative w-full h-[376px] border border-[#B2B2B2] rounded-md bg-[#F6F7FB]'
-                    >
-                        <div className='absolute top-4 right-4'>
-                            {/* <button onClick={handleToggleFavorite} className="absolute top-4 right-4 z-10 p-2 rounded-full bg-white/90 backdrop-blur-sm shadow">
-                              {isFavorite ? <FilledBookmarkIcon /> : <Bookmark />}
-                            </button> */}
-                        </div>
-                        <Image
-                            src={item.image.url}
-                            alt={item.title}
-                            width={387}
-                            height={212}
-                            className='w-full'
-                        />
-                        <div className='flex items-center justify-between mt-4 px-4'>
-                            <h2 className='text-[#333333] text-[17px] font-bold'>{item.title}</h2>
-                            {/* <h2 className='text-[#787676] text-base font-normal'>5 دقائق</h2> */}
-                        </div>
-                        <p
-                          className='text-[#8B8282] text-base mt-3 px-4 line-clamp-2'
-                          dangerouslySetInnerHTML={{ __html: item.content }}
-                        />
-                        <p className='text-[#8B8282] text-base mt-3 px-4 justify-end flex'>
-                          {new Date(item.published_at).toLocaleString('ar-EG', {
-                            dateStyle: 'medium',
-                            timeStyle: 'short',
-                          })}
-                        </p>
-                    </div>
-                    </Link>
-                ))}
-            </div>
-        </section>
-    )
+      <div className='mt-12 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 items-center justify-center gap-[21px]'>
+        {posts?.map((post) => (
+          <BlogCard key={post.id} post={post} />
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default Blogs
--- src/components/blog/BlogsHeader.tsx
+++ src/components/blog/BlogsHeader.tsx
@@ -10,14 +10,14 @@ const BlogsHeader = () => {
     return (
         <div>
             <h1 className='text-[#000000] text-[25px] font-bold flex justify-center'>{t('blog')}</h1>  
-                <div className="flex w-full flex-col gap-6 mt-10">
+                <div className="flex flex-wrap flex-col gap-6 mt-10">
                     <Tabs defaultValue="account">
                         <TabsList className='border-b pb-14'>
-                            <TabsTrigger className='w-32 lg:w-full md:w-40' value="allBlogs">{t('all')}</TabsTrigger>
-                            <TabsTrigger className='w-32 lg:w-full md:w-40' value="advices">{t('advices')}</TabsTrigger>
-                            <TabsTrigger className='w-32 lg:w-full md:w-40' value="tools">{t('tools')}</TabsTrigger>
-                            <TabsTrigger className='w-32 lg:w-full md:w-40' value="stories">{t('stories')}</TabsTrigger>
-                            <TabsTrigger className='w-32 lg:w-full md:w-40' value="directions">{t('direction')}</TabsTrigger>
+                            <TabsTrigger className='w-44 lg:w-full md:w-52' value="allBlogs">{t('all')}</TabsTrigger>
+                            <TabsTrigger className='w-44 lg:w-full md:w-52' value="advices">{t('advices')}</TabsTrigger>
+                            <TabsTrigger className='w-44 lg:w-full md:w-52' value="tools">{t('tools')}</TabsTrigger>
+                            <TabsTrigger className='w-44 lg:w-full md:w-52' value="stories">{t('stories')}</TabsTrigger>
+                            <TabsTrigger className='w-44 lg:w-full md:w-52' value="directions">{t('direction')}</TabsTrigger>
                         </TabsList>
                     </Tabs>
                 </div>
