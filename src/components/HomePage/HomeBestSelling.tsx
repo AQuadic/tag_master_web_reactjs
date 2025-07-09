77a8c17d4be1a077f1826fc1afcd3bb046e3d602
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-07-09 12:29:49 +0300

    home products - translation

--- src/components/HomePage/HomeBestSelling.tsx
+++ src/components/HomePage/HomeBestSelling.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Arrow from "../icons/home/Arrow";
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import EmptyState from "../general/EmptyState";
 import { addToCart } from "@/api/cart/addToCart";
 import { toast } from "sonner";
@@ -22,7 +22,7 @@ interface HomeBestSellingProps {
 
 const HomeBestSelling = ({ data }: HomeBestSellingProps) => {
   const t = useTranslations("bestProducts");
-
+  const locale = useLocale ();
   const handleAddToCart = async (productId: number) => {
     try {
       await addToCart("product", productId);
@@ -86,10 +86,10 @@ const HomeBestSelling = ({ data }: HomeBestSellingProps) => {
                 />
                 <div className="w-full h-full border border-[#D9D9D9] rounded-md py-4 px-8">
                   <h2 className="text-[#000000] text-[21px] font-bold">
-                    {item.name.ar}
+                    {item.name?.[locale]}
                   </h2>
                   <p className="lg:w-[731px] text-[#464B4E] text-[17px] mt-2.5">
-                    {item.description.ar}
+                    {item.description?.[locale]}
                   </p>
                   <p className="mt-4">{item.price}</p>
                   <div className="flex items-center justify-end mt-4">
@@ -111,7 +111,7 @@ const HomeBestSelling = ({ data }: HomeBestSellingProps) => {
                       onClick={() => handleAddToCart(item.id)}
                       className="w-[160px] h-11 bg-[#2F3437] text-white text-base rounded-[39px]"
                     >
-                      + إضافة سريعة
+                      {t('quickAdd')}
                     </button>
                   </div>
                 </div>
