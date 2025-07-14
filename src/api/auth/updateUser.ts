eb79718294370bd71a1c68b63a7540038f767e01
ahmed <ahmedabozaid.dev@gmail.com>
2025-07-14 15:36:49 +0300

    fixes upload image

--- src/api/auth/updateUser.ts
+++ src/api/auth/updateUser.ts
@@ -5,11 +5,24 @@ interface UpdateUserPayload {
   email?: string;
   phone?: string;
   phone_country?: string;
+  profile_image?: File;
 }
 
 export const updateUser = async (payload: UpdateUserPayload) => {
+  const formData = new FormData();
 
-  const response = await axios.post("/user/update", payload, {
+  // Add text fields to FormData only if they exist
+  if (payload.name !== undefined) formData.append("name", payload.name);
+  if (payload.email !== undefined) formData.append("email", payload.email);
+  if (payload.phone !== undefined) formData.append("phone", payload.phone);
+  if (payload.phone_country !== undefined)
+    formData.append("phone_country", payload.phone_country);
+  if (payload.profile_image) formData.append("image", payload.profile_image);
+
+  const response = await axios.post("/user/update", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
   });
 
   return response.data;
