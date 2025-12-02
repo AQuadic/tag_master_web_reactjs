017e474bbdbd4cab9f66427e317e06851926ee36
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-12-02 14:02:59 +0200

    fix build

--- src/components/HomePage/HomeBlogs.tsx
+++ src/components/HomePage/HomeBlogs.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { getPosts } from '@/api/blogs/getBlogs'
+import { getPosts, PostsApiResponse } from '@/api/blogs/getBlogs'
 import { useQuery } from '@tanstack/react-query'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -15,75 +15,86 @@ const HomeBlogs = () => {
     const t = useTranslations("homeblogs");
 
     const {
-    data: posts,
+    data: postsResponse,
     isLoading,
-    } = useQuery<PostType[]>({
-        queryKey: ['posts'],
-        queryFn: getPosts,
-    });
+    } = useQuery<PostsApiResponse>({
+    queryKey: ["posts", 1], 
+    queryFn: () => getPosts(1),
+  });
 
-    console.log("Blogs are: ", posts)
-
-    if (isLoading) return <div className='flex justify-center my-10'>
+  if (isLoading)
+    return (
+      <div className="flex justify-center my-10">
         <Spinner />
-    </div>
-    
-    const featuredPosts = posts?.filter(item => item.is_featured);
+      </div>
+    );
+  const posts: PostType[] = postsResponse?.data ?? [];
+
+  const featuredPosts = posts.filter((item) => item.is_featured);
 
     return (
-        <section className='container mt-20'>
-            <div className='flex flex-col items-center'>
-                <Link href='/blog' className='text-[#007CC2] text-[17px]'>{t('readMore')}</Link>
-                <h2 className='text-[#000000] text-[25px] font-bold'>{t('blog')}</h2>
+        <section className="container mt-20">
+        <div className="flex flex-col items-center">
+            <Link href="/blog" className="text-[#007CC2] text-[17px]">
+            {t("readMore")}
+            </Link>
+            <h2 className="text-[#000000] text-[25px] font-bold">{t("blog")}</h2>
+        </div>
+
+        <div className="mt-[46px] flex 2xl:flex-row flex-col justify-between">
+            <Image
+            src="/images/home/blogs/blog.png"
+            alt="blog banner"
+            width={729}
+            height={501}
+            className="md:w-[829px] w-full"
+            />
+            <div className="mt-4 2xl:mt-0">
+            <div className="flex items-center justify-between">
+                <h2 className="text-[#000000] text-[25px] font-bold">
+                {t("latestArticles")}
+                </h2>
+                <Link href="/blog" className="flex items-center gap-3">
+                <p className="text-[#000000] text-base font-semibold">
+                    {t("seeMore")}
+                </p>
+                <Arrow />
+                </Link>
             </div>
 
-            <div className='mt-[46px] flex 2xl:flex-row flex-col  justify-between'>
-                <Image 
-                    src='/images/home/blogs/blog.png' 
-                    alt='blog banner'
-                    width={729}
-                    height={501}
-                    className='md:w-[829px] w-full'
+            {posts.slice(0, 4).map((item, index) => (
+                <div
+                key={index}
+                className="md:w-[395px] w-full h-[98px] bg-[#F6F7FB] rounded-[12px] mt-4 flex items-center justify-between gap-[13px] py-4 px-4"
+                >
+                <Image
+                    src={item.image.url}
+                    alt="blog image"
+                    width={66}
+                    height={66}
                 />
-                <div className='mt-4 2xl:mt-0'>
-                    <div className='flex items-center justify-between'>
-                        <h2 className='text-[#000000] text-[25px] font-bold'>{t('latestArticles')}</h2>
-                        <Link href='/blog' className="flex items-center gap-3">
-                            <p className="text-[#000000] text-base font-semibold">
-                            {t("seeMore")}
-                            </p>
-                            <Arrow />
-                        </Link>
-                    </div>
-                    {posts?.slice(0, 4).map((item, index) => (
-                        <div
-                            key={index}
-                            className='md:w-[395px] w-full h-[98px] bg-[#F6F7FB] rounded-[12px] mt-4 flex items-center justify-between gap-[13px] py-4 px-4'
-                        >
-                            <Image
-                                src={item.image.url}
-                                alt='blog image'
-                                width={66}
-                                height={66}
-                            />
-                            <div>
-                                <h2 className='text-[#333333] text-[17px] font-bold'>{item.title}</h2>
-                                <p
-                                    className='text-[#8B8282] text-base mt-3 line-clamp-2'
-                                    dangerouslySetInnerHTML={{ __html: item.content }}
-                                />                            
-                            </div>
-                            <p className='text-[#787676] text-base'>{new Date(item.published_at).toLocaleDateString('ar-EG')}</p>
-                        </div>
-                    ))}
+                <div>
+                    <h2 className="text-[#333333] text-[17px] font-bold">
+                    {item.title}
+                    </h2>
+                    <p
+                    className="text-[#8B8282] text-base mt-3 line-clamp-2"
+                    dangerouslySetInnerHTML={{ __html: item.content }}
+                    />
                 </div>
-            </div>
-
-           <div className='mt-12 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 items-center justify-center gap-[21px]'>
-            {featuredPosts?.map((item) => (
-                <FeaturedBlogCard key={item.id} post={item} />
+                <p className="text-[#787676] text-base">
+                    {new Date(item.published_at).toLocaleDateString("ar-EG")}
+                </p>
+                </div>
             ))}
             </div>
+        </div>
+
+        <div className="mt-12 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 items-center justify-center gap-[21px]">
+            {featuredPosts.map((item) => (
+            <FeaturedBlogCard key={item.id} post={item} />
+            ))}
+        </div>
         </section>
     )
 }
